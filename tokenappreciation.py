eae506ca2c860e6619aa10d344af344727161510
ElMario90 <93047445+ElMario90@users.noreply.github.com>
2024-10-15 09:39:02 +0100

    Add files via upload

---
+++ tokenappreciation.py
@@ -0,0 +1,10 @@
+import jwt
+
+# The JWT provided
+encoded_jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmbGFnIjoiY3J5cHRve2p3dF9jb250ZW50c19jYW5fYmVfZWFzaWx5X3ZpZXdlZH0iLCJ1c2VyIjoiQ3J5cHRvIE1jSGFjayIsImV4cCI6MjAwNTAzMzQ5M30.shKSmZfgGVvd2OSB2CGezzJ3N6WAULo3w9zCl_T47KQ"
+
+# Decoding the JWT without verifying the signature, specifying the algorithm used
+decoded_jwt = jwt.decode(encoded_jwt, options={"verify_signature": False}, algorithms=["HS256"])
+
+# Print the decoded JWT
+print(decoded_jwt)
