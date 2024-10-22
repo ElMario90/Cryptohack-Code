41402d8ae8ee9e77cfcfa06fe1cb574f25d9d618
ElMario90 <93047445+ElMario90@users.noreply.github.com>
2024-10-22 02:39:14 +0100

    Add files via upload

---
+++ EULERSTOTIENT.py
@@ -0,0 +1,9 @@
+# Given values for p and q from the problem
+p = 857504083339712752489993810777
+q = 1029224947942998075080348647219
+
+# Euler's Totient Function for N = p * q is calculated as (p - 1) * (q - 1)
+totient_N = (p - 1) * (q - 1)
+
+# Output the result
+print(totient_N)
